b0c1857a87351839530c64dd639de3ebc7459a62
KevinBett <kevinbett458@gmail.com>
2023-03-20 01:01:06 +0300

    Added functionality  for the gradeGenerator button

---
+++ script.js
@@ -0,0 +1,25 @@
+function checkGrade(){
+  const marks = Number(prompt("Enter student marks(between 0 and 100): " ))
+
+if (isNaN(marks) || marks < 0 || marks > 100) {
+  alert("Please enter a valid mark between 0 and 100.");
+  return;
+}
+
+let grade;
+if (marks >=80 && marks<=100){
+  grade = "A";
+} else if (marks >= 60 && marks <= 79 ){
+  grade = "B";
+} else if (marks >= 50 && marks <= 59 ){
+  grade = "C"
+} else if (marks >= 40 && marks <= 49 ){
+  grade = "D"
+} else if (marks >= 0 && marks <= 39 ){
+  grade = "E"
+} 
+
+alert("Grade: " + grade)
+}
+
+
